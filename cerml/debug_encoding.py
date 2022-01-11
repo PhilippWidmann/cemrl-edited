f1948870ae6f76a08a1b6cc47e7dce41c6e5930c
PhilippWidmann <38128497+PhilippWidmann@users.noreply.github.com>
2022-01-11 12:57:04 +0100

    add mutual info computation for encoding debugging

--- cerml/debug_encoding.py
+++ cerml/debug_encoding.py
@@ -4,10 +4,102 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+import pandas as pd
+from pandas.api.types import is_integer_dtype, is_float_dtype, is_categorical_dtype
+import sklearn
+from sklearn.neighbors import BallTree
+from scipy.special import digamma
 
 import rlkit.torch.pytorch_util as ptu
 from rlkit.torch.networks import Mlp
 from rlkit.core import logger
+
+
+def mutual_information(x, y, n_neighbors=5):
+    if is_integer_dtype(x.dtype) or is_categorical_dtype(x.dtype):
+        if is_integer_dtype(y.dtype) or is_categorical_dtype(y.dtype):
+            return np.log2(np.e) * max(sklearn.metrics.mutual_info_score(x, y), 0.0)
+        elif is_float_dtype(y.dtype):
+            x = pd.array(x, dtype='category')
+            return mutual_information_mixed(y, x, normalization=False, n_neighbors=n_neighbors)
+        else:
+            raise ValueError(f'Datatype {y.dtype} cannot be identified as continuous or discrete '
+                             f'(for computing mutual information).')
+    elif is_float_dtype(x.dtype):
+        if is_integer_dtype(y.dtype) or is_categorical_dtype(y.dtype):
+            y = pd.array(y, dtype='category')
+            return mutual_information_mixed(x, y, normalization=False, n_neighbors=n_neighbors)
+        elif is_float_dtype(y.dtype):
+            return mutual_information_continuous(x, y, n_neighbors=n_neighbors)
+        else:
+            raise ValueError(f'Datatype {y.dtype} cannot be identified as continuous or discrete '
+                             f'(for computing mutual information).')
+    else:
+        raise ValueError(f'Datatype {x.dtype} cannot be identified as continuous or discrete '
+                         f'(for computing mutual information).')
+
+
+def mutual_information_mixed(x, classes, normalization=False, n_neighbors=5):
+    if classes.ndim != 1:
+        raise ValueError("Discrete variable must be one-dimensional.")
+    if classes.shape[0] != x.shape[0]:
+        raise ValueError("First dimension of discrete and continuous variable must match.")
+
+    if x.ndim == 1:
+        x = np.reshape(x, (-1, 1))
+
+    nn_full = BallTree(x, metric='chebyshev')
+    n = x.shape[0]
+    d = np.zeros(classes.shape)
+    n_classes = np.zeros(len(classes.categories))
+
+    for i, c in enumerate(classes.categories):
+        n_classes[i] = np.sum(classes == c)
+        nn_class = BallTree(x[classes == c], metric='chebyshev')
+        # BallTree considers a point its own closest neighbor, so use n_neighbors+1
+        d_temp, _ = nn_class.query(x[classes == c], k=n_neighbors + 1, return_distance=True)
+        d[classes == c] = d_temp[:, -1]
+
+    # BallTree counts a point as its own neighbor. Thus subtract 1.
+    m = nn_full.query_radius(x, r=d, count_only=True) - 1
+
+    mutual_info = np.log2(np.e) * (digamma(n) - np.average(digamma(n_classes), weights=n_classes) +
+                                   digamma(n_neighbors) - np.average(digamma(m)))
+
+    if normalization:
+        class_count = np.array([np.sum(classes == c) for c in classes.categories])
+        class_prob = class_count / np.sum(class_count)
+        discriminator_entropy = - np.sum(class_prob * np.log2(class_prob))
+        mutual_info = mutual_info / discriminator_entropy
+
+    return max(mutual_info, 0.0)
+
+
+def mutual_information_continuous(x, y, n_neighbors=5):
+    if x.shape[0] != y.shape[0]:
+        raise ValueError("First dimension of variables must match.")
+    n = x.shape[0]
+
+    if x.ndim == 1:
+        x = np.reshape(x, (-1, 1))
+    if y.ndim == 1:
+        y = np.reshape(y, (-1, 1))
+
+    z = np.concatenate((x, y), axis=1)
+
+    nn_z = BallTree(z, metric='chebyshev')
+    nn_x = BallTree(x, metric='chebyshev')
+    nn_y = BallTree(y, metric='chebyshev')
+
+    dist, _ = nn_z.query(z, k=n_neighbors+1, return_distance=True)
+    # Algorithm requires finding points with strictly smaller distance; -1E-16 is an approximation
+    dist = dist[:, -1] - 1E-16
+    n_x = nn_x.query_radius(x, r=dist, count_only=True) - 1
+    n_y = nn_y.query_radius(y, r=dist, count_only=True) - 1
+
+    mutual_info = np.log2(np.e) * (digamma(n_neighbors) - np.mean(digamma(n_x + 1)) - np.mean(digamma(n_y + 1))
+                                   + digamma(n))
+    return max(mutual_info, 0.0)
 
 
 class DecoderOnlyLatent(nn.Module):
@@ -95,12 +187,39 @@ class EncodingDebugger:
     def record_debug_info(self, decoder_reconstruction_steps):
         self.train_decoder(decoder_reconstruction_steps)
         self.validate_z_dependency()
+        self.compute_mi_scores()
+
+    def compute_mi_scores(self):
+        all_ind, ind_temp = self.replay_buffer.get_train_val_indices(self.train_val_percent)
+        all_ind = np.concatenate((all_ind, ind_temp))
+
+        val_batch_size = min(int(2 ** 12), len(all_ind))
+
+        data = self.replay_buffer.sample_random_few_step_batch(all_ind, val_batch_size, normalize=True)
+
+        encoder_input = self.replay_buffer.make_encoder_data(data, self.batch_size)
+        y_distribution, z_distribution = self.encoder.encode(encoder_input)
+        z, y = self.encoder.sample_z(y_distribution, z_distribution, y_usage='most_likely', sampler='mean')
+        z, y = ptu.get_numpy(z), ptu.get_numpy(y)
+
+        true_task_base = np.array([d[-1, 0]['base_task'] for d in data['true_tasks']])
+        true_task_spec = pd.array([d[-1, 0]['specification'] for d in data['true_tasks']], dtype='category')
+        r = data['rewards'][:, -1, :]
+
+        logger.record_tabular("MI(z, true_task_base)", mutual_information(z, true_task_base))
+        logger.record_tabular("MI(y, true_task_base)", mutual_information(y, true_task_base))
+
+        logger.record_tabular("MI(z, true_task_spec)", mutual_information(z, true_task_spec))
+        logger.record_tabular("MI(y, true_task_spec)", mutual_information(y, true_task_spec))
+
+        logger.record_tabular("MI(z, reward)", mutual_information(z, r))
+        logger.record_tabular("MI(y, reward)", mutual_information(y, r))
 
     def validate_z_dependency(self):
         all_ind, ind_temp = self.replay_buffer.get_train_val_indices(self.train_val_percent)
         all_ind = np.concatenate((all_ind, ind_temp))
         np.random.shuffle(all_ind)
-        val_batch_size = int(2**14)
+        val_batch_size = int(2**12)
 
         reward_loss = torch.tensor(0.0, device=ptu.device)
         for i in range(int(np.ceil(all_ind.size / val_batch_size))):
@@ -155,7 +274,6 @@ class EncodingDebugger:
 
         logger.record_tabular("Only_latent_reconstruction_train_val_reward_loss", validation_train)
         logger.record_tabular("Only_latent_reconstruction_val_reward_loss", validation_val)
-        logger.record_tabular("Only_latent_reconstruction_epochs", epoch + 1)
 
     def decoder_training_step(self, indices, epoch):
         data = self.replay_buffer.sample_random_few_step_batch(indices, self.batch_size, normalize=True)
