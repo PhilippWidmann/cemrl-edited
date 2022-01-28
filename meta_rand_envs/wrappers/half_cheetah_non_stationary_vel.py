8f67f6d5cd6fb5332b7393c9600459cceb95fe54
PhilippWidmann <38128497+PhilippWidmann@users.noreply.github.com>
2022-01-28 16:17:47 +0100

    add some modified cheetah-stationary-vel envs

--- meta_rand_envs/wrappers/half_cheetah_non_stationary_vel.py
+++ meta_rand_envs/wrappers/half_cheetah_non_stationary_vel.py
@@ -3,6 +3,7 @@ from meta_rand_envs.half_cheetah_non_stationary_velocity import HalfCheetahNonSt
 from . import register_env
 
 @register_env('cheetah-stationary-vel')
+@register_env('cheetah-stationary-vel-oneTimestep')
 @register_env('cheetah-non-stationary-vel')
 class HalfCheetahNonStationaryVelWrappedEnv(HalfCheetahNonStationaryVelocityEnv):
     def __init__(self, *args, **kwargs):
